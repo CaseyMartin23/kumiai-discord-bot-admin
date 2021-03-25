f89daeb29ba3555dcc22f934e615dd326a023450
CaseyMartin23 <cm232311@gmail.com>
2021-03-25 11:16:54 +0200

    .env change

---
+++ client/build/precache-manifest.ff1a2ddb62e4c13090c46ec082a3546d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3baa8b19ff8062c0759fdda3efcc0cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096638c600c4b28cac18",
+    "url": "/static/css/2.21ee2aab.chunk.css"
+  },
+  {
+    "revision": "071e70c98fe3478052c6",
+    "url": "/static/css/main.2336a868.chunk.css"
+  },
+  {
+    "revision": "096638c600c4b28cac18",
+    "url": "/static/js/2.8ab22b9c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.8ab22b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071e70c98fe3478052c6",
+    "url": "/static/js/main.c0f728d7.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
